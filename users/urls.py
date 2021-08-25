778f10abc69fe0f7a5637250f8d827508ebaaf82
Anton-Varava <varava.anton@gmail.com>
2021-08-25 14:35:22 +0300

    Added user change password.

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.shortcuts import render
+
 from . import views
 
 
 urlpatterns = [
     path('<int:pk>/edit', views.UserEditView.as_view(), name='edit-user'),
     path('<int:pk>', views.UserDetailView.as_view(), name='profile-user'),
+    path('<int:pk>/password', views.UserPasswordChangeView.as_view(), name='change-password'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.forms import PasswordChangeForm
 from django.urls import reverse_lazy, reverse
 from .forms import UserRegistrationForm, UserUpdateForm
 # from django.contrib.auth.models import Group, User
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import PermissionDenied
 
 from users.models import User
 
@@ -17,18 +21,35 @@ class UserCreateView(CreateView):
         return reverse('users:profile-user', args=(self.object.id, ))
 
 
-class UserEditView(UpdateView):
+class UserEditView(SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserUpdateForm
     template_name = 'users/user_form.html'
+    success_message = 'Profile was changed successfully...'
 
     def get_success_url(self):
         pk = self.kwargs['pk']
         return reverse('users:profile-user', kwargs={'pk': pk})
 
+    def get_object(self, queryset=None):
+        obj = super(UserEditView, self).get_object(queryset)
+        if self.request.user == obj or self.request.user.is_staff:
+            return obj
+        raise PermissionDenied
+
 
 class UserDetailView(DetailView):
     model = User
     template_name = 'users/user_detail.html'
+
+
+class UserPasswordChangeView(SuccessMessageMixin, PasswordChangeView):
+    form_class = PasswordChangeForm
+    template_name = 'users/change-password.html'
+    success_message = 'Your Password was changed successfully...'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse('users:profile-user', kwargs={'pk': pk})
 
 
